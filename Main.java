f707fa0dc37d27930acfb06b9f22ac4f78421a6e
Lautaro Ruiz <101646833+Lautaro-Ruiz@users.noreply.github.com>
2023-05-27 17:45:55 -0300

    Add files via upload
    Clases basicas creadas de persona, usuario y admin

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> zonas = new ArrayList<>();
+        zonas.add("Barrio 1");
+        zonas.add("Barrio 2");
+        Usuario usuario = new Usuario("Lautaro", "Ruiz", "2235601139", 18, "lautaroruiz04", "45905505" ,zonas, "Lauti");
+
+    }
+
+}
