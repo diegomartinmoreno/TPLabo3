fa7b92dca50384d8e29c9a8a76361629f89cd6e4
Roqueotha04 <101643928+Roqueotha04@users.noreply.github.com>
2023-06-17 17:49:41 -0300

    Add files via upload

--- src/model/Zonas.java
+++ src/model/Zonas.java
@@ -1,5 +1,6 @@
 package model;
 
 public enum Zonas {
+    ///zonas limitadas que trabaja la app
     PUERTO,CENTRO,ALEM,RUMENCO,CONSTITUCION,INDEPENDENCIA, LOS_TRONCOS, COLINAS, BOSQUE, MOGOTES;
 }
