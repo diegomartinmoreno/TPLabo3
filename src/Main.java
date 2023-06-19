54d9cc0e5004034d5b89d13688171f0fd6e59148
Roqueotha04 <roqueotha04@gmail.com>
2023-06-19 16:38:49 -0300

    Que lindo es estar en mar del plata, en alpargata, todos en pata

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        /// la verga me excita si no tiene pelos
         PedidosYa pedidosYa = new PedidosYa();
         Carrito carrito = new Carrito();
         HistorialDeCompras historialDeCompras = new HistorialDeCompras();
