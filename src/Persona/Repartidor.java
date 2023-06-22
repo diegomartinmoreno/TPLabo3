11c17b3d923f1e1b1bd0c61ea7e6eff3cf551d76
Moya Ramiro <moya04.ramiro@gmail.com>
2023-06-22 19:00:30 -0300

    finikit

---
+++ src/Persona/Repartidor.java
@@ -0,0 +1,72 @@
+package Persona;
+import Persona.Persona;
+import model.Carrito;
+import model.Tarjeta;
+
+import java.util.Scanner;
+
+public class Repartidor extends Persona {
+	final double gananciaPorPedido = 500;
+	private double TotalCobradoPorRepartidor=0;
+	
+	public Repartidor() {
+	}
+
+	public Repartidor(String nombre, String apellido, String nroDeTelefono, int edad, String email, String dni) {
+		super(nombre, apellido, nroDeTelefono, edad, email, dni);
+	}
+
+	public boolean cargarRepartidor(Carrito pedidoATrasladar, Scanner scan) {
+		boolean flag=true;
+		do {
+			System.out.println("Ingrese nombre del repartidor:");
+			String lectura;
+			lectura=scan.nextLine();
+			if (!Tarjeta.verificaEsNumero(lectura)) {
+				flag=true;
+			}
+		}while (flag);
+
+		
+		this.TotalCobradoPorRepartidor=0;
+		
+		return flag;
+	}
+
+	public double getGananciaPorPedido() {
+		return gananciaPorPedido;
+	}
+
+	public void cobrarLoDelDia(){
+		System.out.println("El repartidor " + getNombre() + " se lleva una ganancia de $" + getTotalCobradoPorRepartidor());
+	}
+
+	public void setTotalCobradoPorRepartidor(double totalCobradoPorRepartidor) {
+		TotalCobradoPorRepartidor = totalCobradoPorRepartidor;
+	}
+
+
+	public double getTotalCobradoPorRepartidor() {
+		return TotalCobradoPorRepartidor;
+	}
+
+	public void llevarPedido(){
+		this.TotalCobradoPorRepartidor+=this.gananciaPorPedido;
+	}
+
+	public void mostrarRepartidor(){
+		System.out.println(getNombre() + " " + getApellido());
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() +
+				"Repartidor{" +
+				"gananciaPorPedido=" + gananciaPorPedido +
+				", TotalCobradoPorRepartidor=" + TotalCobradoPorRepartidor +
+				'}';
+	}
+}
+	
+	
+
--- src/model/Carrito.java
+++ src/model/Carrito.java
@@ -1,10 +1,12 @@
 package model;
 
+import Persona.Repartidor;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Carrito {
@@ -17,22 +19,16 @@ public class Carrito {
     private static final double PORCENTAJE_DESCUENTO = 0.85;
     private boolean tieneCupon=false;
     private Repartidor repartidor;
-
-    ///////////////////////////////////
     private double montoTotal=0;
     private Zonas destino=null;
-    /////////////////////////////////// campos esenciales para el historial
 
     public Carrito() {
         productos = new ColeccionGenerica<>();
         fechaPedido = LocalDate.now().toString();
     }
 
-    
-
 
     //1
-
     public boolean agregarProductoAlCarrito(Empresa vendedor, Producto producto) throws NullPointerException{
         if(producto==null) throw new NullPointerException("Error! El producto no puede ser nulo.//***");
 
@@ -43,8 +39,6 @@ public class Carrito {
 
 
     //2
-
-
     public void eliminarProductoDelCarrito(int pos) throws RuntimeException {
         if (pos > -1 && pos <= productos.getProductos().size()) {
             productos.remove(pos);
@@ -65,7 +59,7 @@ public class Carrito {
 
     }
 
-
+    //4 en PedidosYa
 
     //5
     public void pagarCarrito (HistorialDeCompras historial, Tarjeta tarjeta, Scanner scanner) throws NullPointerException{
@@ -110,54 +104,6 @@ public class Carrito {
         } while (!flag);
     }
 
-    public void pasarProductosAlPedido(Pedido pedido){
-        for(Producto producto : productos.getProductos()){
-            pedido.agregarProducto(producto);
-        }
-    }
-
-    public Repartidor generarRepartidorAleatorio(){
-        return new Repartidor("Leo", "Messi", "2235212344", 17, "liomessi@gmail.com", "45621234");
-    }
-
-    public void mostrarProductos(){
-        int i=0;
-        System.out.println("Productos del carrito: ");
-
-        for (Producto producto : productos.getProductos()){
-            System.out.println(i + ".///  " + producto);
-            i++;
-        }
-    }
-
-    public List<Producto> getProductos() {
-        return productos.getProductos();
-    }
-
-    public void setNota(String nota) {
-        this.nota = nota;
-    }
-
-    public void setMontoTotal(double montoTotal) {
-        this.montoTotal = montoTotal;
-    }
-
-    public double calcularMontoTotalDeLaCompra(){
-        double montoTotal =0;
-        for (Producto producto : productos.getProductos()){
-            montoTotal = montoTotal + (producto.getPrecio() * producto.getCantidadPedido());
-        }
-
-        montoTotal += vendedor.getCostoDeEnvio();
-
-        if (tieneCupon){
-            System.out.println("Usted posee un descuento del 15%!!");
-            montoTotal *= PORCENTAJE_DESCUENTO;
-        }
-
-        return montoTotal;
-    }
-
     //6
     public void mostrarProductosDelCarrito(){
         System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
@@ -178,6 +124,72 @@ public class Carrito {
         }
         System.out.println("Fecha del pedido: " + fechaPedido + "\n");
     }
+
+    public void pasarProductosAlPedido(Pedido pedido){
+        for(Producto producto : productos.getProductos()){
+            pedido.agregarProducto(producto);
+        }
+    }
+
+    public Repartidor generarRepartidorAleatorio(){
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(7);
+
+        switch (numeroAleatorio){
+            case 0 -> {
+                return new Repartidor("Guillermo", "Martinez", "2231235431", 30, "guilleprofe@gmail.com", "35002123");
+            }
+            case 1 -> {
+                return new Repartidor("Dani", "Ayudante", "2232938154", 23, "daniayudante@gmail.com", "40231456");
+            }
+            case 2 -> {
+                return new Repartidor("Lautaro", "Ruiz", "2234789234", 19, "lautiruiz@gmail.com", "45627384");
+            }
+            case 3 -> {
+                return new Repartidor("Diego", "Martin", "2234892134", 23, "diego@gmail.com", "43758491");
+            }
+            case 4 -> {
+                return new Repartidor("Roque", "Otahcehe", "2237865943", 19, "roque@gmail.com", "44521345");
+            }
+            case 5 -> {
+                return new Repartidor("Ramiro", "Moya", "2236578945", 19, "rama@gmail.com", "45741473");
+            }
+            case 6 -> {
+                return new Repartidor("Leo", "Messi", "2235212344", 17, "liomessi@gmail.com", "45621234");
+            }
+            default -> {
+                System.out.println("COMO LLEGASTE ACA??????!!!!");
+                return new Repartidor("Fausto", "Moya", "2236789341", 24, "faustoelmascap@gmail.com", "40213456");
+            }
+        }
+    }
+
+    public void mostrarProductos(){
+        int i=0;
+        System.out.println("Productos del carrito: ");
+
+        for (Producto producto : productos.getProductos()){
+            System.out.println(i + ".///  " + producto);
+            i++;
+        }
+    }
+
+    public double calcularMontoTotalDeLaCompra(){
+        double montoTotal =0;
+        for (Producto producto : productos.getProductos()){
+            montoTotal = montoTotal + (producto.getPrecio() * producto.getCantidadPedido());
+        }
+
+        montoTotal += vendedor.getCostoDeEnvio();
+
+        if (tieneCupon){
+            System.out.println("Usted posee un descuento del 15%!!");
+            montoTotal *= PORCENTAJE_DESCUENTO;
+        }
+
+        return montoTotal;
+    }
+
 
     public int buscarProductoPorNombre(String nombre) throws NullPointerException, RuntimeException{
         if(nombre==null) throw new NullPointerException("Error! La cadena no puede ser nula.//***");
@@ -202,6 +214,17 @@ public class Carrito {
         vendedor=null;
     }
 
+    public List<Producto> getProductos() {
+        return productos.getProductos();
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    public void setMontoTotal(double montoTotal) {
+        this.montoTotal = montoTotal;
+    }
     public String getFechaPedido() {
         return fechaPedido;
     }
